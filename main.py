8c0a3d66d8e3decf98af487af7f657429c347b77
Jakob <jakob.seidl@gmx.de>
2020-09-14 17:46:15 +1000

    Changes added and commited in pyCharm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import imports
 
-print('Hello world - This was edited online....')
+print('Hello world - This was edited online....'
+      'And this was added in pyCharm!'
+      '')
 
 A = imports.CreditCard(3442)
 A.getSecondDigits()
